3e8061b5bdb470c4d6672846e07baa66b3fb3586
AJ <ahmedmusaafir@gmail.com>
2018-11-20 15:25:44 +0100

    Add download frameworks script

---
+++ download_frameworks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+mkdir frameworks
+
+cd frameworks && wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz && tar -xvzf zookeeper-3.4.8.tar.gz
+
+cd ../
+
+cd frameworks && wget https://archive.apache.org/dist/hadoop/core/hadoop-2.6.0/hadoop-2.6.0.tar.gz && tar -xvzf hadoop-2.6.0.tar.gz
